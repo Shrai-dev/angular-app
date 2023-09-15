6faf0e1160799c3838cf734fb10ee13074a470b8
Anastasia <nastia.shrai@gmail.com>
2023-09-15 21:11:18 +0300

    feat: implement routing and add global styles

--- weather-app/src/app/app-routing.module.ts
+++ weather-app/src/app/app-routing.module.ts
@@ -1,5 +1,13 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { UsersComponent } from './components/users/users.component';
+import { UsersInfoComponent } from './components/users-info/users-info.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'users-list', pathMatch: 'full' },
+  { path: 'users-list', component: UsersComponent },
+  { path: 'users-info', component: UsersInfoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- weather-app/src/app/app.component.ts
+++ weather-app/src/app/app.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IUser } from './components/services/users.interfaces';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  static favoriteUsers: IUser[] = [];
+  constructor() {}
+
+  ngOnInit(): void {
+    this.checkSavedUsers();
+  }
+
+  checkSavedUsers() {
+    if (localStorage.getItem('favoriteUsers') !== null) {
+      AppComponent.favoriteUsers = JSON.parse(
+        localStorage.getItem('favoriteUsers')!
+      );
+    }
+  }
+}
